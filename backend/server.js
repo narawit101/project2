3ff6c206660e2975a1b72efbc3fc7174a1f082a5
narawit <narawit.so@rmuti.ac.th>
2025-06-22 23:21:29 +0700

    domain

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,9 @@ const server = http.createServer(app);
 
 const allowedOrigins = [
   "http://localhost:3000",
-  "https://project-eight-jet-70.vercel.app", // เปลี่ยนให้ตรงกับของคุณจริงๆ
+  "https://project-eight-jet-70.vercel.app", 
+  "https://sport-hub-70.vercel.app/", 
+  // เปลี่ยนให้ตรงกับของคุณจริงๆ
 ];
 
 const io = new Server(server, {
