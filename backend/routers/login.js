8a1f49b0444b720c5173b17ebd5c7a6c71470438
narawit <narawit.so@rmuti.ac.th>
2025-06-25 15:28:31 +0700

    cookie

--- backend/routers/login.js
+++ backend/routers/login.js
@@ -13,14 +13,14 @@ router.post("/", async (req, res) => {
   console.log("Request protocol:", req.protocol);
   console.log("NODE_ENV:", process.env.NODE_ENV);
   console.log("Hostname:", req.hostname);
-  console.log("x-forwarded-proto:", req.headers["x-forwarded-proto"]);
+  console.log("Secue", req.secure);
   console.log("User-Agent:", req.headers["user-agent"]);
 
   // ตรวจว่าเป็น Mobile หรือ Desktop จาก User-Agent
   const isMobile = /Mobile|Android|iPhone|iPad/i.test(
     req.headers["user-agent"]
   );
-  console.log("🟨 Device Type:", isMobile ? "📱 Mobile" : "💻 Desktop");
+  console.log("Device Type:", isMobile ? "📱 Mobile" : "💻 Desktop");
 
   try {
     const userQuery = `SELECT * FROM users WHERE user_name = $1 OR email = $1`;
@@ -52,7 +52,7 @@ router.post("/", async (req, res) => {
       { expiresIn: "5h" }
     );
     const isProd = process.env.NODE_ENV === "production";
-    const isHttps = req.headers["x-forwarded-proto"] === "https";
+    const isHttps = req.secure;
 
     res.cookie("token", token, {
       httpOnly: true,
@@ -64,12 +64,12 @@ router.post("/", async (req, res) => {
     const responseData = {
       message: "เข้าสู่ระบบสำเร็จ",
     };
+
     if (isMobile) {
       responseData.token = token;
     }
 
     return res.status(200).json(responseData);
-    
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ message: "เกิดข้อผิดพลาด", error: error.message });
